bc60612aad60c03a6d3d2e2d74be9b6c011c6f73
WulanTsania <151324955+WulanTsania@users.noreply.github.com>
2024-01-12 15:07:58 +0700

    uas

---
+++ Nomor2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<vector>
+#include<algoritma>
+using namespace std;
+
+void main() {
+	int kotak[10] = { 87, 45, 50, 25, 32, 10, 75, 90, 5, 8 };
+	vector<int>vec(kotak, kotak, +sizeof(kotak) / sizeof(int);
+
+	cout << 
+}
